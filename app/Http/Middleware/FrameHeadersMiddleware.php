76d3c51c3396ae6e376b8c0a46c6706c16da3fa8
nmaiss <nicolas.maissouradze@gmail.com>
2021-02-25 15:31:04 +0100

    add middleware

--- app/Http/Middleware/FrameHeadersMiddleware.php
+++ app/Http/Middleware/FrameHeadersMiddleware.php
@@ -1,11 +1,22 @@
 <?php
 
 namespace App\Http\Middleware;
+
 use Closure;
 
-public function handle($request, Closure $next)
+class FrameHeadersMiddleware
 {
-     $response = $next($request);
-     $response->header('X-Frame-Options', 'ALLOW FROM https://freegolf.fr/');
-     return $response;
- }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->header('X-Frame-Options', 'ALLOW FROM https://freegolf.fr/');
+        return $response;
+    }
+}
